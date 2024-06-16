118fdf6a9859762b73e7f5256206ca575264d6f8
wanderson648 <testepedroca@gmail.com>
2024-06-16 10:48:01 -0300

    inicia aplicacao

---
+++ src/main/java/com/wo/geranciamentotarefas/GerenciamentoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.wo.geranciamentotarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wo/geranciamentotarefas/GeranciamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.geranciamentotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeranciamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
